e410e35c3fa48b92982597fb6c2cf5a2be220a6f
Stacy <smbridson@gmail.com>
2018-11-07 16:01:01 -0500

    added Foursquare API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map';
+import SquareAPI from './API/';
 
 class App extends Component {
+  componentDidMount() {
+    SquareAPI.search({
+      near: "Lexington, KY",
+      query: "horse",
+      limit: 17
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
